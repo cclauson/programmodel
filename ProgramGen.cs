39981d9e86076bc11e7c9df324ca1b80ea6c5667
cclauson <ceclauson@hotmail.com>
2016-07-28 00:15:53 -0700

    Completed basic version of graph conversion code

--- ProgramGen.cs
+++ ProgramGen.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 
 namespace ProgramModel
 {
@@ -40,23 +41,41 @@ namespace ProgramModel
 			}
 		}
 
-		private class ProgramImpl
+		private class ProgramImpl : Program<MutationT, ConditionT>
 		{
-			private BasicBlock initialBlock;
-			private IList<BasicBlock> allBlocks;
+			private ProgramNode initialBlock;
 
-			public ProgramImpl(BasicBlock initialBlock, IList<BasicBlock> allBlocks)
+			public ProgramImpl(ProgramNode initialBlock)
 			{
 				this.initialBlock = initialBlock;
-				this.allBlocks = allBlocks.ToImmutableList();
 			}
 
 		}
 
-		private interface ProgramSubgraph
+		//this could easily be an interface but I
+		//only ended up subtyping this once
+		private class ProgramSubgraph
 		{
-			ProgramNode GetEntryPoint();
-			void SetExitDest(ProgramNode programNode);
+			private readonly ProgramNode entryPoint;
+			private readonly Action<ProgramNode> exitDestSetter;
+
+			public ProgramSubgraph(ProgramNode programNode, Action<ProgramNode> exitDestSetter)
+			{
+				if(programNode == null) throw new ArgumentNullException("programNode is null");
+				if(exitDestSetter == null) throw new ArgumentNullException("exitDestSetter is null");
+				this.entryPoint = programNode;
+				this.exitDestSetter = exitDestSetter;
+			}
+
+			public ProgramNode GetEntryPoint()
+			{
+				return entryPoint;
+			}
+
+			public void SetExitDest(ProgramNode programNode)
+			{
+				this.exitDestSetter (programNode);
+			}
 		}
 
 		//for information associated with a loop
@@ -94,9 +113,11 @@ namespace ProgramModel
 			private void setNextProgramNode(ProgramNode programNode, Action<ProgramNode> setNextNode)
 			{
 
-				if (programNode is BasicBlock) {
-					throw new ArgumentException ("Basic block node not allowed");
-				}
+				//commented because in the case of do-while, we
+				//need to jump directly to the body
+				//if (programNode is BasicBlock) {
+				//	throw new ArgumentException ("Basic block node not allowed");
+				//}
 
 				if (this.initial == null) {
 					initial = programNode;
@@ -267,23 +288,56 @@ namespace ProgramModel
 					}
 				}
 			}
-		}
 
+			//returns null if the block passed was empty
+			//otherwise, returns a ProgramSubgraph that
+			//can be used to get the entry point for the
+			//subgraph, and can set the 
+			public ProgramSubgraph AsProgramSubgraph()
+			{
+				Action<ProgramNode> setter;
+				if (initial == null) {
+					return null;
+				} else if (bb != null) {
+					setter = delegate(ProgramNode programNode) {
+						bb.coda = programNode;
+					};
+				} else if (setNextNode != null) {
+					setter = setNextNode;
+				} else {
+					throw new Exception ("Initial non-null, but bb and setNextNode both null, but this should never happen");
+					//Debug.Fail ("Initial non-null, but bb and setNextNode both null, but this should never happen");
+					//return null; //never execu
+				}
+				return new ProgramSubgraph (initial, setter);
+			}
+
+		}
+			
 		private static ProgramSubgraph processCodeBlock(CodeBlock<MutationT, ConditionT> cb, Dictionary<Loop, LoopRecord> loopLabelMap)
 		{
-
 			//code that would theoretically go here moved to instance method of
 			//ProgramGraphBuilder, so that subroutining can be used
 			ProgramGraphBuilder pgb = new ProgramGraphBuilder ();
 			pgb.buildFromCodeBlock (cb, loopLabelMap);
-
-			//TODO: Convert current status of pgb to ProgramSubgraph
-			return null;
+			//NOTE: This will return null if the block
+			//was empty
+			return pgb.AsProgramSubgraph ();
 		}
 
-		private ProgramImpl ToProgram()
+		public Program<MutationT, ConditionT> ToProgram()
 		{
-			return null;
+			Dictionary<Loop, LoopRecord> loopLabelMap = new Dictionary<Loop, LoopRecord> ();
+			ProgramSubgraph mainProgramGraph = processCodeBlock (this, loopLabelMap);
+			ProgramNode rootProgramNode;
+			if (mainProgramGraph == null) {
+				//the entire block is empty, so main node is "return" node
+				rootProgramNode = PROGRAM_RETURN;
+			} else {
+				mainProgramGraph.SetExitDest (PROGRAM_RETURN);
+				rootProgramNode = mainProgramGraph.GetEntryPoint ();
+			}
+			return new ProgramImpl (rootProgramNode);
 		}
 
 	}
