2b60867ed8672408ff51357dc5f32bbe962f82ed
cclauson <ceclauson@hotmail.com>
2016-07-28 00:14:42 -0700

    Added actual test code

--- TestMain.cs
+++ TestMain.cs
@@ -8,10 +8,57 @@ namespace ProgramModel
 
 		static void Main()
 		{
-			System.Console.WriteLine ("Hello");
+			//System.Console.WriteLine ("Hello");
 
 			CodeBlock<String, String> codeBlock = new CodeBlock<String, String> ();
+
+			codeBlock.addMutation ("mutation1");
+			codeBlock.addMutation ("mutation2");
+			codeBlock.addMutation ("mutation3");
+
+			CodeBlock<String, String> ifBody = codeBlock.addIf ("ifcond1");
+			ifBody.addMutation ("mutation4");
+			ifBody.addMutation ("mutation5");
+			ifBody.addMutation ("mutation6");
+
+			codeBlock.addMutation ("mutation7");
+			codeBlock.addMutation ("mutation8");
+			codeBlock.addMutation ("mutation9");
+
+			CodeBlock<String, String> whileBody;
+
+			//whileBody = codeBlock.addWhile("whilecond1");
+
+			Tuple<CodeBlock<String, String>, Loop> whileRes = codeBlock.addWhile ("whilecond1", "mywhile");
+			whileBody = whileRes.Item1;
+			Loop mywhile = whileRes.Item2;
+
+			whileBody.addMutation ("mutation10");
+			whileBody.addMutation ("mutation11");
+
+			CodeBlock<String, String> innerDoWhileBody = whileBody.addDoWhile("doWhileCond");
+			innerDoWhileBody.addMutation ("mutation17");
+			innerDoWhileBody.addMutation ("mutation18");
+			innerDoWhileBody.addMutation ("mutation19");
+
+			Tuple<CodeBlock<String, String>, CodeBlock<String, String>> ifElseResult = innerDoWhileBody.addIfElse ("ifelsecond");
+
+
+
+			innerDoWhileBody.addBreak (mywhile);
+			innerDoWhileBody.addMutation ("mutation20");
+			innerDoWhileBody.addMutation ("mutation21");
+
+			whileBody.addMutation ("mutation12");
+			whileBody.addMutation ("mutation13");
+
+			codeBlock.addMutation ("mutation14");
+			codeBlock.addMutation ("mutation15");
+			codeBlock.addMutation ("mutation16");
+
 			System.Console.WriteLine (codeBlock);
+
+			codeBlock.ToProgram ();
 
 		}
 
